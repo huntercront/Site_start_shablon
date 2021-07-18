836c20714112745ac5785748ff81e7120b59fc2e
huntercront <38252870+huntercront@users.noreply.github.com>
2021-07-18 08:55:41 +0300

    add template

---
+++ js/app.js
@@ -0,0 +1,53 @@
+WebFontConfig = {
+    google: { families: ['Inter:400,600,700'] }
+};
+
+(function(d) {
+    var wf = d.createElement('script'),
+        s = d.scripts[0];
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+    wf.async = true;
+    s.parentNode.insertBefore(wf, s);
+})(document);
+
+
+//loader function
+var Loader = function() {}
+Loader.prototype = {
+    require: function(scripts, callback) {
+        this.loadCount = 0;
+        this.totalRequired = scripts.length;
+        this.callback = callback;
+
+        for (var i = 0; i < scripts.length; i++) {
+            this.writeScript(scripts[i]);
+        }
+    },
+    loaded: function(evt) {
+        this.loadCount++;
+
+        if (this.loadCount == this.totalRequired && typeof this.callback == 'function') this.callback.call();
+    },
+    writeScript: function(src) {
+        var self = this;
+        var s = document.createElement('script');
+        s.type = "text/javascript";
+        s.defer = true;
+        s.src = src;
+        s.addEventListener('load', function(e) { self.loaded(e); }, false);
+        var head = document.getElementsByTagName('head')[0];
+        head.appendChild(s);
+    }
+}
+
+
+var l = new Loader();
+l.require([
+        //"src to load"
+    ],
+    function() {
+
+        //script for load
+
+
+    });
